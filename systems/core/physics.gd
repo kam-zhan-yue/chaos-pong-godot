9194758ff24203a846e5fdac4ebb61fb5c7850c7
kamzhanyue <kamzhanyue@gmail.com>
2024-07-16 23:51:13 +0900

    Make ball bounce using existing functions

--- systems/core/physics.gd
+++ systems/core/physics.gd
@@ -1,6 +1,8 @@
 extends Node
 
 const GRAVITY = Vector2(0, 200)
+const BLOCK_HEIGHT = 16.0
+const BLOCK_WIDTH = 32.0
 
 # Calculate y as start.y - end.y due to Godot's handling of coordinate system
 func get_projectile_speed(start: Vector2, end: Vector2) -> float:
@@ -15,7 +17,6 @@ func get_projectile_angle(start: Vector2, end: Vector2) -> float:
 	var x := end.x - start.x
 	var y := start.y - end.y
 	var a := y/x
-	var flipped_angle: float
 	if x >= 0:
 		return rad_to_deg(PI * 0.5 - 0.5 * (PI * 0.5 - atan(a)))
 	else:
@@ -26,9 +27,11 @@ func get_projectile_time(start: Vector2, end: Vector2, v: float, angle: float) -
 	var vX := v * cos(deg_to_rad(angle))
 	var time := x / vX
 	return time
+
+func get_velocity(speed: float, angle: float) -> Vector2:
+	var angle_rad := deg_to_rad(angle)
+	return Vector2(speed * cos(angle_rad), -speed * sin(angle_rad))
 	
 func simulate_position(start: Vector2, speed: float, angle: float, a: Vector2, t: float) -> Vector2:
-	var angle_rad := deg_to_rad(angle)
-	var u := Vector2(speed * cos(angle_rad), -speed * sin(angle_rad))
-	var v := u + a * t
+	var u := get_velocity(speed, angle)
 	return start + u * t + a * (0.5 * t * t)
--- systems/ping_pong/ball.gd
+++ systems/ping_pong/ball.gd
@@ -18,6 +18,15 @@ func _physics_process(delta: float) -> void:
 		var next_position := Physics.simulate_position(start, launch_speed, launch_angle, Physics.GRAVITY, launch_timer)
 		global_position = next_position
 		launch_timer += delta
+	elif launch_time > 0.0:
+		bounce()
+
+func bounce() -> void:
+	# Calculate new end position and offset by height of block
+	start = end
+	end = Physics.simulate_position(start, launch_speed, launch_angle, Physics.GRAVITY, launch_time)
+	end.y += Physics.BLOCK_HEIGHT
+	launch(end)
 
 func launch(target: Vector2) -> void:
 	start = global_position
@@ -26,7 +35,7 @@ func launch(target: Vector2) -> void:
 	launch_speed = Physics.get_projectile_speed(start, end)
 	launch_angle = Physics.get_projectile_angle(start, end)
 	launch_time = Physics.get_projectile_time(start, end, launch_speed, launch_angle)
-	launch_timer = 0.1
+	launch_timer = 0
 	
 	print("Ball %v, Table %v" %[start, end])
 	print("Velocity %f, Angle %f, Time %f" %[launch_speed, launch_angle, launch_time])
